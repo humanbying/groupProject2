ab4ae9310ba96498f268603f28072bf5b0e83ca8
Brian Ying <humanbying@gmail.com>
2016-08-19 19:28:28 -0700

    "routes and models + folders"

---
+++ server/routes/players.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const Player = require('../models/player');
+
+router.route('/')
+  .get((req, res) => {
+    Player.find({}, (err, players) => {
+      res.status(err ? 400 : 200 ).send(err || players);
+    })
+  })
+  .post((req, res) => {
+    Player.create( req.body, (err, newPlayer) => {
+      res.status(err ? 400 : 200 ).send(err || newPlayer);
+    })
+  })
+
+router.route('/:id')
+  .get((req, res) => {
+    Player.findById(req.params.id, (err, player) => {
+      res.status(err ? 400 : 200 ).send(err || player);
+    })
+  })
+  .delete((req, res) => {
+    Player.findByIdAndRemove(req.params.id, (err) => {
+      res.status(err ? 400 : 200 ).send(err || req.params.id);
+    })
+  })
+  .put((req, res) => {
+    Player.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, updatedPlayer) => {
+      res.status(err ? 400 : 200 ).send(err || updatedPlayer);
+    })
+  })
+
+module.exports = router;
